e36b47f690422bcf33d07632cf8fabe69e6a764f
Abodawoud <yousefabodawoud@gmail.com>
2023-10-06 14:38:00 +0300

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,26 +16,20 @@ def do_deploy(archive_path):
         return False
 
     try:
-        put(archive_path, '/tmp/')
+        li = archive_path.split('/')
+        file_name = li[-1]
+        releases_path = "/data/web_static/releases/{}/".format(file_name[:-4])
+        tmp_path = "/tmp/{}".format(file_name)
 
-        file_name = os.path.basename(archive_path)
-        release_folder = f"/data/web_static/releases/{file_name[:-4]}"
-
-        run(f'mkdir -p {release_folder}/')
-
-        run(f'tar -xzf /tmp/{file_name} -C {release_folder}/')
-
-        run(f'rm /tmp/{file_name}')
-
-        run(f'mv {release_folder}/web_static/* {release_folder}/')
-
-        run(f'rm -rf {release_folder}/web_static/')
-
-        run('rm -rf /data/web_static/current/')
-
-        run(f'ln -s {release_folder}/ /data/web_static/current/')
-
-        print('New version deployed!')
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
         return True
     except Exception as e:
         return False
