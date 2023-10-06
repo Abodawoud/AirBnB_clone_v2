3744c90208e4e42cc8deab8fa7edfe7c92033090
Abodawoud <yousefabodawoud@gmail.com>
2023-10-06 14:13:55 +0300

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,47 +1,41 @@
 #!/usr/bin/python3
-"""
-Fabric script that distributes an archive to your web servers
-"""
-from fabric.api import local, env, put, run
-from os.path import exists
-from datetime import datetime
+from fabric.api import *
+import os
+"""Deploying"""
+
 
 env.hosts = ['52.87.230.156', '100.26.151.67']
+
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to the web servers
-    """
-
-    if not exists(archive_path):
+    """Deploy web_static"""
+    if not os.path.exists(archive_path):
         return False
 
     try:
         put(archive_path, '/tmp/')
 
-        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-        release_folder = "/data/web_static/releases/{}".format(timestamp)
-        run("mkdir -p {}".format(release_folder))
+        file_name = os.path.basename(archive_path)
+        release_folder = f"/data/web_static/releases/{file_name[:-4]}"
 
-        archive_filename = archive_path.split("/")[-1]
-        run("tar -xzf /tmp/{} -C {}".format(archive_filename, release_folder))
+        run(f'mkdir -p {release_folder}/')
 
-        run("rm /tmp/{}".format(archive_filename))
+        run(f'tar -xzf /tmp/{file_name} -C {release_folder}/')
 
-        run("mv {}/web_static/* {}".format(release_folder, release_folder))
+        run(f'rm /tmp/{file_name}')
 
-        run("rm -rf {}/web_static".format(release_folder))
+        run(f'mv {release_folder}/web_static/* {release_folder}/')
 
-        run("rm -rf /data/web_static/current")
+        run(f'rm -rf {release_folder}/web_static')
 
-        run("ln -s {} /data/web_static/current".format(release_folder))
+        run('rm -rf /data/web_static/current')
 
-        print("New version deployed!")
+        run(f'ln -s {release_folder}/ /data/web_static/current')
+
+        print('New version deployed!')
         return True
-
     except Exception as e:
-        print(e)
         return False
